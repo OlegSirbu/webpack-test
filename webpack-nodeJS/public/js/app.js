7ff5f88a84fd175aea287c73ab8ed485d8cdfb8d
Oleg Syrbu <olegsirbu2@gmail.com>
2016-10-23 11:26:43 +0300

    added webpack with express node js

---
+++ webpack-nodeJS/public/js/app.js
@@ -0,0 +1,3 @@
+console.log('app js file');
+
+require('../css/style.less');
---
+++ webpack-nodeJS/webpack.config.js
@@ -0,0 +1,69 @@
+var path = require('path');
+
+module.exports = {
+    context: path.resolve('public/js'),
+    entry: "./app.js",
+    output: {
+        path: path.resolve('build/'),
+        publicPath: '/public/assets/',
+        filename: 'bundle.js'
+    },
+    devServer:{
+        contentBase: 'public'
+    },
+    module: {
+        preLoaders:[
+            {
+                test: /\.js$/,
+                exlude: /node_modules/,
+                loader: "jshint-loader"
+            }
+        ],
+        loaders: [
+            {
+                test: /\.scss$/,
+                exlude: /node_modules/,
+                loader: "style-loader!css-loader!sass-loader"
+            },
+            {
+                test: /\.less$/,
+                exlude: /node_modules/,
+                loader: "style-loader!css-loader!less-loader"
+            },
+            {
+                test: /\.es6$/,
+                exlude: /node_modules/,
+                loader: "babel-loader"
+            },
+            {
+                test: /\.css$/,
+                exlude: /node_modules/,
+                loader: "style-loader!css-loader"
+            },
+            {
+                test: /\.(png|jpg)$/,
+                loader: "url-loader?limit=100000"
+            },
+            {
+                test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/,
+                loader: 'url?limit=10000&mimetype=application/font-woff'
+            },
+            {
+                test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+                loader: 'url?limit=10000&mimetype=application/octet-stream'
+            },
+            {
+                test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+                loader: 'file'
+            },
+            {
+                test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+                loader: 'url?limit=10000&mimetype=image/svg+xml'
+            }
+        ]
+    },
+
+    resolve: {
+        extensions: ['', '.es6', '.js']
+    }
+};
