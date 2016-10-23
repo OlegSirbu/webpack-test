b849cbfcd3dd3baaf3766b7675b2ccb4d8ae3a05
Oleg Syrbu <olegsirbu2@gmail.com>
2016-10-23 10:10:25 +0300

    added fonts/ img

--- webpack.config.js
+++ webpack.config.js
@@ -38,7 +38,28 @@ module.exports = {
             {
                 test: /\.css$/,
                 exlude: /node_modules/,
-                loader: "style-loader!css-loader"}
+                loader: "style-loader!css-loader"
+            },
+            {
+                test: /\.(png|jpg)$/,
+                loader: "url-loader?limit=100000"
+            },
+            {
+                test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/,
+                loader: 'url?limit=10000&mimetype=application/font-woff'
+            },
+            {
+                test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+                loader: 'url?limit=10000&mimetype=application/octet-stream'
+            },
+            {
+                test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+                loader: 'file'
+            },
+            {
+                test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+                loader: 'url?limit=10000&mimetype=image/svg+xml'
+            }
         ]
     },
 
